c46a67df161400055b42656aab59f7fca2c57cd4
pabloaav <pabloaav@PabloAV>
2018-08-28 11:09:18 -0300

    agrego comentario de respuesta

--- Ejemplo1/src/Prueba/holaPablo.java
+++ Ejemplo1/src/Prueba/holaPablo.java
@@ -15,7 +15,9 @@ public class holaPablo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola pablo");
-
+		//Respuesta
+		System.out.println("hola mauro");
+	
 	}
 
 }
